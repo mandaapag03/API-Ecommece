fd84c05d7530c6b15a9a1576d08c676223f88a74
mandaapag03 <paganiamanda791@gmail.com>
2024-04-12 23:01:01 -0300

    delete endpoint delete inventory

--- InventoryAPI/InventoryAPI/Model/Interfaces/IInventoryRepository.cs
+++ InventoryAPI/InventoryAPI/Model/Interfaces/IInventoryRepository.cs
@@ -6,6 +6,6 @@
         Task<Inventory> Get(int productId);
         Task<Inventory> Add(Inventory inventory);
         Task<Inventory> UpdateQuantity(Inventory inventory);
-        Task<Inventory> Delete(int productId);
+        // Task<Inventory> Delete(int productId);
     }
 }
